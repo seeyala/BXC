a19d16512fe8e62d3058252cb83ecc9d32935188
Viet <0209.nhuviet@gmail.com>
2024-10-02 20:45:15 +0700

    update

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+// src/app/login/page.tsx
+import LoginComponent from '../components/LoginComponent';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default LoginPage;
